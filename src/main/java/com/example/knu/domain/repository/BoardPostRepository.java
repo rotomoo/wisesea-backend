07fc00f71d18d53a1a3b633190de60a2505db607
YunByungil <dbsquddlfz@naver.com>
2023-07-16 18:48:07 +0900

    :sparkles: BoardPostRepository#findAllByBoardCategoryId 메서드 생성

--- src/main/java/com/example/knu/domain/repository/BoardPostRepository.java
+++ src/main/java/com/example/knu/domain/repository/BoardPostRepository.java
@@ -3,5 +3,9 @@ package com.example.knu.domain.repository;
 import com.example.knu.domain.entity.board.BoardPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardPostRepository extends JpaRepository<BoardPost, Long> {
+
+    List<BoardPost> findAllByBoardCategoryId(Long categoryId);
 }
