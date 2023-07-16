a2024c1af4923759f938e5ef4c7eca84ae926813
YunByungil <dbsquddlfz@naver.com>
2023-07-17 01:38:05 +0900

    :sparkles: Response 및 ResultCode 생성

---
+++ src/main/java/com/example/knu/domain/entity/enums/ResultCode.java
@@ -0,0 +1,12 @@
+package com.example.knu.domain.entity.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ResultCode {
+    SUCCESS(HttpStatus.OK);
+    private final HttpStatus httpStatus;
+}
