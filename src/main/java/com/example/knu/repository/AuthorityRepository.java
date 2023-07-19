7e6c3d8a5a75733d9a91cc6d2d7374d6ebafa0c4
Song say <118115640+songsay1224@users.noreply.github.com>
2023-07-19 21:04:01 +0900

    Revert "로그인, 회원가입 기능"
    This reverts commit 93e43705458780e4a2676bb4fefb5881f8f6172d.

--- src/main/java/com/example/knu/repository/AuthorityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.knu.repository;
-
-import com.example.knu.domain.entity.User.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, String> {
-}
