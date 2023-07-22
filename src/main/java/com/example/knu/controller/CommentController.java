9e0d0aac87a8504845299b86f2147e15d472a72a
YunByungil <dbsquddlfz@naver.com>
2023-07-22 15:32:09 +0900

    refactor: 게시글 안에있는 댓글 조회 기능 구현

--- src/main/java/com/example/knu/controller/CommentController.java
+++ src/main/java/com/example/knu/controller/CommentController.java
@@ -3,10 +3,13 @@ package com.example.knu.controller;
 import com.example.knu.common.Response;
 import com.example.knu.dto.comment.request.CommentCreateRequestDto;
 import com.example.knu.dto.comment.response.CommentCreateResponseDto;
+import com.example.knu.dto.comment.response.CommentListResponseDto;
 import com.example.knu.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/api")
 @RequiredArgsConstructor
@@ -19,5 +22,11 @@ public class CommentController {
                                                             @PathVariable Long postId,
                                                             Authentication authentication) {
         return Response.success(commentService.createComment(createDto, postId, authentication.getName()));
+    }
+
+    @GetMapping("/all/boards/categories/{postId}/comments")
+    public Response<List<CommentListResponseDto>> findComment(@PathVariable Long postId) {
+
+        return Response.success(commentService.findComment(postId));
     }
 }
--- src/main/java/com/example/knu/service/CommentService.java
+++ src/main/java/com/example/knu/service/CommentService.java
@@ -8,11 +8,15 @@ import com.example.knu.domain.repository.CommentRepository;
 import com.example.knu.domain.repository.UserRepository;
 import com.example.knu.dto.comment.request.CommentCreateRequestDto;
 import com.example.knu.dto.comment.response.CommentCreateResponseDto;
+import com.example.knu.dto.comment.response.CommentListResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -31,5 +35,18 @@ public class CommentService {
 
         Comment comment = commentRepository.save(createDto.toEntity(boardPost, user, createDto.getCommentContents()));
         return new CommentCreateResponseDto(comment);
+    }
+
+    public List<CommentListResponseDto> findComment(Long postId) {
+//        List<Comment> allByPostId = commentRepository.findAllByPostId(postId);
+
+//        List<CommentListResponseDto> commentListResponseDto = allByPostId.stream()
+//                .map(CommentListResponseDto::new)
+//                .collect(Collectors.toList());
+        List<Comment> allComment = commentRepository.findAllByBoardPostId(postId);
+        List<CommentListResponseDto> commentListResponseDto = allComment.stream()
+                .map(CommentListResponseDto::new)
+                .collect(Collectors.toList());
+        return commentListResponseDto;
     }
 }
