853d063264fc460ee58cfc269aaf6337e17b5b7e
“rotomoo” <quddlf410@naver.com>
2023-07-30 13:32:44 +0900

    fix(UserService) : 닉네임 변경 null 예외 추가

--- src/main/java/com/example/knu/dto/user/NicknameRequest.java
+++ src/main/java/com/example/knu/dto/user/NicknameRequest.java
@@ -1,11 +1,11 @@
 package com.example.knu.dto.user;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class NicknameRequest {
 
-    @NotNull
+    @NotBlank
     private String nickname;
 }
